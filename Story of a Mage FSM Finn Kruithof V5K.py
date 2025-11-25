89f6558b41263e78b29a2e794323f298067ca038
FlinsyK <224217@lekenlinge.nl>
2025-11-25 16:15:20 +0100

    FSM

---
+++ Story of a Mage FSM Finn Kruithof V5K.py
@@ -0,0 +1,167 @@
+states= {
+
+
+    "staat1": {
+        "text": "Je ontwaakt in de ruïnes van Arcanum, het oude centrum van magie. De lucht trilt van de energie, en een vreemd licht schijnt uit een gebroken \033[36;1mkristal\033[m voor je. Je loopt er naar toe nu heb je een keuze om te maken.",
+        "choices": {
+            "raak het kristal aan": "staat1A",
+            "onderzoek de omgeving eerst": "staat1B"
+                   }
+        },
+
+    "staat1A": {
+        "text": "Je loopt er naar toe en je voelt de \033[35;1mmagie\033[m al overal om je heen. Je raakt het \033[36;1mkristal\033[m aan en voelt de \033[35;1mmagie\033[m door je aderen stromen. Je geest vult zich met kennis, maar ook met pijn.",
+        "choices": {
+            "gebruik de nieuwe kracht": "staat2A",
+            "weersta de kracht": "staat2B"
+                     }
+        },
+    
+    "staat1B": {
+        "text": "Je loopt rond en raakt de muren aan dan voel je ineens een losse steen. Je haalt deze er uit en vind tussen de stenen een oud dagboek. Je begint het boek te lezen en je leert over het ritueel dat Arcanum vernietigde en in deze staat deed achterlaten.",
+        "choices": {
+            "lees over het ritueel": "staat3A",
+            "verbrand het boek": "staat3B"
+            
+                   }
+        },
+
+    "staat2A": {
+        "text": "De kracht barst los en daarmee werd ook jouw menselijke vormen de ruïne van Arcanum verwoest. Jij overleeft, maar jij en de wereld om je heen zijn veranderd. Je wordt een levende storm van magie die door de wereld raast om onheil te zaaien. Je wordt een onsterfelijke Mage, maar de mensheid vreest je.",
+        "choices": {
+            "opnieuw starten": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat2B": {
+        "text": "Je kiest om de kracht te weer staan. Het kristal begint te doven, en een portaal verschijnt achter het kristal uit het niks. Het zal waarschijnlijk leiden naar een andere wereld.",
+        "choices": {
+            "stap door de poort": "staat4A",
+            "vernietig de poort": "staat4B"
+            
+                   }
+        },
+    "staat3A": {
+        "text": "Je leest verder over het ritueel en over hoe het bedoeld was om de goden te bereiken. Toen het geprobeerde werd mislukte het helaas, waardoor Arcanum werd verwoest. Jij realiseert je dat jij het kunt afmaken en de goden kan bereiken.",
+        "choices": {
+            "voltooi het ritueel": "staat5A",
+            "laat het rusten": "staat5B"
+            
+                   }
+        },
+    "staat3B": {
+        "text": "Je steekt het boek is brand en het vuur verteert het boek, maar dan als het boek bijna helemaal is vergaan verschijnt er een geest uit het boek. Deze geest is woedend dat je zijn kennis hebt vernietigd. De Geest geeft je een dilemma bevecht hem of onderwerp je aan hem",
+        "choices": {
+            "vecht tegen de geest": "staat6A",
+            "onderwerp je aan hem": "staat6B"
+            
+                   }
+        },
+    "staat4A": {
+        "text": "Je stapt door het portaal. Je verschijnt in een wereld vol zwevende eilanden waar de lucht een andere kleur is. Ineens hoor je een stem die iets blijft herhalen je oren ringen nog dus in het begin versta je het niet maar op een keer hoor je het helder en de stem zegt: “leren of regeren wat gaat het worden?”",
+        "choices": {
+            "leren": "staat7A",
+            "regeren": "staat7B"
+            
+                   }
+        },
+    "staat4B": {
+        "text": "Het portaal vervaagt na een tijdje, terwijl het vervaagt hoor duizenden stemmen die je bedanken. Je hebt zielen bevrijd. Je wordt een bewaarder van rust, een wijze zonder macht maar met vrede.",
+        "choices": {
+            "opnieuw starten": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat5A": {
+        "text": "Je begint aan het ritueel. Je hebt er wat moeite mee maar uit eindelijk is het je gelukt om het te voltooien. Er verschijnt een god die je een belangrijke keuze geeft: kennis of eeuwig leven.",
+        "choices": {
+            "kies kennis": "staat8A",
+            "kies eeuwig leven ": "staat8B"
+            
+                   }
+        },
+    "staat5B": {
+        "text": "Je kiest ervoor om de ruïne te verlaten. Dit doe je dan wel zonder magie, maar met wijsheid over de wonderlijke wereld van de magie. Weet je zeker dat je niet terug wil?",
+        "choices": {
+            "terug arcanum in": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat6A": {
+        "text": "Je begint aan het gevecht met de geest het is een hevig gevecht maar na de felle strijd vernietig je de geest, maar terwijl je de geest afmaakt slaat jij je nog een keer zo hard dat je ziel beschadigd. Je blijft in leven, maar omdat je ziel beschadigt vergeet je naam en verleden. Dus begin je je leven praktisch opnieuw.",
+        "choices": {
+            "opnieuw spelen": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat6B": {
+        "text": "Omdat je jouw lichaam onderwerpt aan de geest neemt de geest bezit van je lichaam. Je wordt een donkere heerser, eeuwig gebonden aan wraak.",
+        "choices": {
+            "opnieuw spelen": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat7A": {
+        "text": "Je studeert eeuwenlang in de andere wereld, tot je de werkelijkheid zelf begrijpt. Door dat je zoveel weet transcendeer je, je bent geen mens meer, maar pure gedachte.",
+        "choices": {
+            "opnieuw spelen": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat7B": {
+        "text": "Je onderwerpt de eilanden en hun bewoners. Je bouwt een rijk in de wolken, maar sterft eenzaam.",
+        "choices": {
+            "opnieuw spelen": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat8A": {
+        "text": "Door de oneindige kennis breekt je verstand.Je wordt een profetische waanzinnige, gevreesd maar gerespecteerd.",
+        "choices": {
+            "opnieuw starten": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat8B": {
+        "text": "Eeuwig leven voelt eerst als een zegen, dan als een vloek. Je ziet de wereld sterven, en blijft alleen achter.",
+        "choices": {
+            "opnieuw starten": "staat1",
+            "stop": "staat9"
+            
+                   }
+        },
+    "staat9": {
+        "text": "Dankjewel voor het spelen van mijn spel ik hoop dat je ervan hebt genoten.",
+        "choices": {}
+        },
+    }
+
+def inputcheck(keuze,keuzes,huidigestaat):
+    if keuze in keuzes:
+        return keuzes[keuze], True
+    else:
+        return huidigestaat, False
+    
+def FSM(staat):
+    while True:
+        print(states[staat]["text"])
+        if states[staat]["choices"]=={}:
+            return None
+        print()
+        print("Uw keuzes zijn:")
+        correctantwoord = False
+        while correctantwoord == False:
+            for keuze in states[staat]["choices"]:
+                print(keuze)
+            invoer = input("wat is uw keuze")
+            staat, correctantwoord = inputcheck(invoer, states[staat]["choices"],staat)
+                
+FSM("staat1")
